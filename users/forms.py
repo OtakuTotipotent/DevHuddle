43ceb2cb23f9a6d6f99a0c87d72fe08ac659fb7d
Afnan Muhammad <otakutotipotent@gmail.com>
2025-12-09 22:07:32 +0500

    fix: updated navigation links, styled signup form, and added secure logout

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("username", "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update(
+                {
+                    "class": "w-full bg-gray-700 text-white border border-gray-600 rounded-lg p-3 focus:outline-none focus:border-blue-500 mb-2",
+                    "placeholder": " ",
+                }
+            )
