8809549c638313ec5e2a4631764f14b1d15ff64b
Afnan Muhammad <otakutotipotent@gmail.com>
2025-12-15 14:16:11 +0500

    feat: enhance user profile dropdown with avatar display and edit/logout options; increase avatar upload size limit to 5MB

--- users/models.py
+++ users/models.py
@@ -1,7 +1,14 @@
+import os
 from django.db import models
-from PIL import Image
 from django.contrib.auth.models import AbstractUser
+from PIL import Image, ImageOps
 from .validators import validate_image_extension, validate_file_size
+
+
+def rename_avatar(instance, filename):
+    extension = filename.split(".")[-1]
+    filename = f"{instance.username}.{extension}"
+    return os.path.join("avatar", filename)
 
 
 class CustomUser(AbstractUser):
@@ -9,11 +16,11 @@ class CustomUser(AbstractUser):
     bio = models.TextField(
         blank=True, null=True, help_text="Tell the world about your stack."
     )
+
     avatar = models.ImageField(
         blank=True,
         null=True,
-        upload_to="avatar/",
-        default="avatars/default.png",
+        upload_to=rename_avatar,
         validators=[validate_file_size, validate_image_extension],
     )
 
@@ -22,8 +29,13 @@ class CustomUser(AbstractUser):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.avatar.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.avatar.path)
+
+        if self.avatar:
+            try:
+                img = Image.open(self.avatar.path)
+                if img.height > 300 or img.width > 300:
+                    output_size = (300, 300)
+                    img = ImageOps.fit(img, output_size, centering=(0.5, 0.5))
+                    img.save(self.avatar.path)
+            except Exception as e:
+                print(f"Image processing failed: {e}")
