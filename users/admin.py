be9ee9bd68114c3b54ed028344ffb8ef66ef1c31
OtakuTotipotent <otakutotipotent@gmail.com>
2026-06-06 16:22:19 +0500

    feat: add models fields to admin panel

--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Skill, Project, Experience
 
-# Register your models here.
+# Standard User Admin
 admin.site.register(CustomUser, UserAdmin)
+
+
+# New Normalized Models
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("title", "user__username")
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ("role", "company", "user", "start_date", "is_current")
+    list_filter = ("is_current", "start_date")
+    search_fields = ("role", "company", "user__username")
